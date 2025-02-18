227fcc14c854f98c3f5689096f7db80febc53402
Miguel Arcanjo <mig.ramos10@gmail.com>
2025-02-18 14:00:20 -0300

    teste do endpoint post

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,12 @@ import { fastifyCors } from "@fastify/cors";
 import {
   validatorCompiler,
   serializerCompiler,
+  ZodTypeProvider,
 } from "fastify-type-provider-zod";
 
-const app = fastify();
+import { z } from "zod";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
@@ -20,10 +23,28 @@ app.register(fastifyCors, {
 // ou
 // app.register(fastifyCors);
 
-// Primeira rota
-app.get("/hello", () => {
-  return "Hello world";
-});
+//body - recursos no corpo da requisição
+//search params - do rota depois do ponto de interrogação, filtros, paginação
+//route params  -  identificação de recursos id.
+app.post(
+  "/subscriptions",
+  {
+    schema: {
+      body: z.object({
+        name: z.string(), // pode ser opcional  .optional()
+        email: z.string().email(),
+      }),
+    },
+  },
+  (request, reply) => {
+    const { name, email } = request.body;
+
+    return reply.status(201).send({
+      name,
+      email,
+    });
+  }
+);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
