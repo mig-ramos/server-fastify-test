479a1655ec6918c5b0abbca7de7b759a7aca664c
Miguel Arcanjo <mig.ramos10@gmail.com>
2025-02-18 13:25:01 -0300

    configura validação e serialização

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
+import {
+  validatorCompiler,
+  serializerCompiler,
+} from "fastify-type-provider-zod";
 
 const app = fastify();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
 app.register(fastifyCors, {
   origin: "http://localhost:3000",
@@ -11,7 +18,7 @@ app.register(fastifyCors, {
 //     origin: true,
 //   });
 // ou
-app.register(fastifyCors);
+// app.register(fastifyCors);
 
 // Primeira rota
 app.get("/hello", () => {
