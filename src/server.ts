4be5acda281635ee1c614de5c0eba15f41886036
Miguel Arcanjo <mig.ramos10@gmail.com>
2025-02-18 13:10:03 -0300

    configura cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
 import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
 
 const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "http://localhost:3000",
+});
+// URLs front ends acesse o server
+// app.register(fastifyCors, {
+//     origin: true,
+//   });
+// ou
+app.register(fastifyCors);
 
 // Primeira rota
 app.get("/hello", () => {
